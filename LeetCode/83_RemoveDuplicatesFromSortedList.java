45ef0be04ebab8c476a7a05d272221e69bcd3432
RichMillions1 <richmillionsmore@gmail.com>
2024-02-20 13:54:21 +0000

    Add comment

--- LeetCode/83_RemoveDuplicatesFromSortedList.java
+++ LeetCode/83_RemoveDuplicatesFromSortedList.java
@@ -7,13 +7,13 @@ class Solution {
         ListNode cur = head;
         while(cur != null && cur.next != null){
             if(cur.val == cur.next.val){
-                cur.next = cur.next.next;
+                cur.next = cur.next.next;//If the value of the current is equal to the value of the next Node, point the pointer of the current Node tho the next next Node
             }else{
-                cur = cur.next;
+                cur = cur.next; //If not the same, move directly to the next Node
             }
         }
      
-        return dummyNode.next;
+        return dummyNode.next; //return the next node of dummy node
 
     }
 }
